d6518d1bcb5bc41b75ef01c30bd448f78f70ee9f
Hrishikesh Suresh <30840337+redlegblackarm@users.noreply.github.com>
2017-10-13 00:31:07 +0530

    fixed almost all errors
    Add isEmpty() function. Also try to add more functions.

--- linked_list.c
+++ linked_list.c
@@ -28,7 +28,7 @@ void delete_element(List* list, char teacher_name[]);
 void disp_list(List* list);
 
 //Delete the linked list along with any qes it has.
-void delete_list(List* list);
+List* delete_list(List* list);
 
 int main()
 {
@@ -37,11 +37,12 @@ int main()
 	char *name;
 	char line[30];
 	FILE *fp = fopen("teachers_data.csv","r+");
-	while(choice > 0 && choice < 4)
+	while(choice > 0 && choice < 5)
 	{
-		printf("1. Auto enter the details of teachers \n");
-		printf("2. Delete a record of teacher\n");
-		printf("3. Display the list \n");
+		printf("1. Auto enter the details of teachers.\n");
+		printf("2. Delete a record of teacher.\n");
+		printf("3. Display the list.\n");
+		printf("4. Delete list.\n");
 	  scanf("%d",&choice);
 		switch(choice)
 		{
@@ -50,20 +51,20 @@ int main()
 									  char *subject;
 									 	name=strtok(line,",");
 										subject=strtok(NULL,",");
-										puts(name);
-										puts(subject);
 										insert_beginning(list,name);
 								 }
 								 break;
 				case 2 : printf("Enter the name of the teacher to be removed from the list\n");
 				         scanf("%s",&name);
-				         delete_element(list, name);
+				         delete_element(list,&name);
 				         break;
 				case 3 : disp_list(list);
 				     		 break;
-		  }
-		fclose(fp);
+				case 4 : delete_list(list);
+								 break;
 		}
+	}
+	fclose(fp);
 }
 List* create_list()
 {
@@ -72,20 +73,20 @@ List* create_list()
     list->no_of_teachers=0;
     return list;
 }
-void insert_beginning(List* list,char teacher_name[])
+void insert_beginning(List* list,char  teacher_name[])
 {
     Node* p=(Node*)malloc(sizeof(Node));
 		int i = 0;
-		printf("\n**check**\n");
-    for(i=0;i<strlen(teacher_name);i++)
+    for(i=0;i<=strlen(teacher_name)-1;i++)
 			p->teacher_name[i]=teacher_name[i];
     p->link=list->head;
     list->head=p;
+		printf("%s\n",p->teacher_name );
     list->no_of_teachers+=1;
 }
 void delete_element(List *list,char teacher_name[])
 {
-  Node *p,*temp;
+  Node *p,*q;
   if(list->head==NULL)
   {
     printf("No elements present in the linked list.\n");
@@ -93,26 +94,34 @@ void delete_element(List *list,char teacher_name[])
   else
   {
     p=list->head;
-    while(p->link!=NULL)
+		q=p;
+    while(p!=NULL)
     {
-      if(strcmp(p->teacher_name,teacher_name)==0)
-      {
-        p->link=NULL;
-        break;
-      }
-   }
-}
+      	if(strcmp(p->teacher_name,teacher_name)==0)
+      	{
+					q->link = p->link;
+					printf("%s has been deleted.\n",p->teacher_name);
+					free(p);
+					list->no_of_teachers--;
+      	}
+				if(p!=NULL)
+				{
+					q = p;
+					p = p->link;
+				}
+		}
+  }
 }
 void disp_list(List* list)
 {
     Node* p=list->head;
     while(p!=NULL)
-    {
-        printf("%s \n",p->teacher_name);
+			{
+      	printf("%s \n",p->teacher_name);
         p=p->link;
-	  }
+	  	}
 }
-void delete_list(List* list)
+List* delete_list(List* list)
 {
     Node* p=list->head;
     Node* q=NULL;
@@ -122,4 +131,5 @@ void delete_list(List* list)
         p=p->link;
         free(q);
     }
+		return list;
 }
