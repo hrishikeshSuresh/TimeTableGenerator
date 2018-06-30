22bc184498590bfd9591e8eed62e2923f8c00479
Hrishikesh Suresh <30840337+redlegblackarm@users.noreply.github.com>
2018-06-30 14:03:23 +0530

    pushed for probot

---
+++ dfs_bfs.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+void DFS_recurse(int v,int *visited){
+	visited[v] = 1;
+	for(i=0; i<n; i++){
+		if
+	}
+}
+
+void DFS_main(int **graph,int n){
+	int visited[n];
+	for(i=0; i<n; i++){
+		visited[i] = 0;
+	}
+	for(i=1; i<=n; ++i){
+			if(visited[i] == 0)
+				dfs_recurse(i);
+	} 
+}
